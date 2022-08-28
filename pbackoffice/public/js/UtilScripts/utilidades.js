e96e2ef4146b8009486ba9c76140014c536ac150
Alberto Monzon <callefalsaservicio@gmail.com>
2022-08-28 12:40:13 -0300

    Actualizacion

--- pbackoffice/public/js/UtilScripts/utilidades.js
+++
@@ -1,5 +0,0 @@
-function ComaPorPunto(id){
-    var valor = $("#"+id).val();
-    var valor = valor.replace(",",".");
-    $("#" + id).val(valor);
-}
--- pcliente/src/Interceptors/axios.js
+++
@@ -1,3 +0,0 @@
-import axios from 'axios';
-
-axios.defaults.baseURL = 'https://pokeapi.co/api/v2';
